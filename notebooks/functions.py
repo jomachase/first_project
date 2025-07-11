c348792bd3e04df95528b77f6a7e0546406e9828
Ignacio Soteras <ignacio.soteras@gmail.com>
2025-07-11 17:17:58 +0200

    Updated version of functions.py

--- notebooks/functions.py
+++ notebooks/functions.py
@@ -1,1 +1,20 @@
-#
+import pandas as pd
+
+def clean_gender(gender: str) -> str:
+	if gender in ["M", "F"]:
+		return gender
+	elif gender in ["m", "male", "Male"]:
+		return "M"
+	elif gender in ["f", "female", "Female"]:
+		return "F"
+	else:
+		return "Unknown"
+
+def clean_colum_names(df: pd.DataFrame) -> pd.DataFrame:
+	df2 = df.copy()
+	
+	df2.columns = [ col.replace(" ","_") for col in df2.columns ]
+
+	return df2
+
+
